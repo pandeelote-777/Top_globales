c98314d1cb420b495cc5c2d66229e2be7a99830a
pandeelote-777 <andrearuizmagana@gmail.com>
2025-11-28 18:47:04 -0600

    Crear formularios

---
+++ GermanXChino/GermanXChino/Form3.Designer.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: andre
+ * Fecha: 28/11/2025
+ * Hora: 06:45 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace GermanXChino
+{
+	partial class Form3
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// Form3
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "Form3";
+			this.Name = "Form3";
+		}
+	}
+}
---
+++ GermanXChino/GermanXChino/Form4.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: andre
+ * Fecha: 28/11/2025
+ * Hora: 06:46 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GermanXChino
+{
+	/// <summary>
+	/// Description of Form4.
+	/// </summary>
+	public partial class Form4 : Form
+	{
+		public Form4()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
